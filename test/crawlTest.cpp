bce10e24ac78916cea63667385eeeb9c4b153e4a
kostikan <ea56@mail.ru>
2020-03-17 16:58:29 +0300

    travis

--- test/crawlTest.cpp
+++ test/crawlTest.cpp
@@ -24,16 +24,19 @@ TEST(case1,test1){
 
     char *paths = get_current_dir_name();
     char *testFolder = (char*)"/files";
-    int len = getLastOf(paths);
+    int len = strlen(paths);
     char *path = (char*)malloc(sizeof(char) * (len + strlen(testFolder)));
-    memcpy(path,paths,strlen(paths));
-    memcpy(path + len,testFolder, len + strlen(testFolder));
+    memcpy(path,paths,len * sizeof(char));
+    len = getLastOf(paths);
+    memcpy(path + len ,testFolder, strlen(testFolder) * sizeof(char));
 
-    Top *top1;
+    Top *top1 = NULL;
     Crawl("kokekrewrkwel",path,&top1);
-    TopPrl *top2;
+    TopPrl *top2 = NULL;
     CrawlPrl("kokekrewrkwel",path,&top2);
     ASSERT_EQ(top1[0].levDistValue, top2[0].levDistValue);
+    free(paths);
+    free(path);
     free(top1);
     free(top2);
 }
@@ -42,16 +45,20 @@ TEST(case2,test2)
 {
     char *paths = get_current_dir_name();
     char *testFolder = (char*)"/files";
-    int len = getLastOf(paths);
+    int len = strlen(paths);
     char *path = (char*)malloc(sizeof(char) * (len + strlen(testFolder)));
-    memcpy(path,paths,strlen(paths));
-    memcpy(path + len,testFolder, len + strlen(testFolder));
+    memcpy(path,paths,len * sizeof(char));
+    len = getLastOf(paths);
+    memcpy(path + len ,testFolder, strlen(testFolder) * sizeof(char));
+
 
     Top *top1;
     Crawl("kokekrewrkwel",path ,&top1);
     TopPrl *top2;
     CrawlPrl("kokekrewrkwel",path,&top2);
     ASSERT_EQ(top1[1].levDistValue, top2[1].levDistValue);
+    free(paths);
+    free(path);
     free(top1);
     free(top2);
 }
@@ -60,16 +67,20 @@ TEST(case3,test3)
 {
     char *paths = get_current_dir_name();
     char *testFolder = (char*)"/files";
-    int len = getLastOf(paths);
+    int len = strlen(paths);
     char *path = (char*)malloc(sizeof(char) * (len + strlen(testFolder)));
-    memcpy(path,paths,strlen(paths));
-    memcpy(path + len,testFolder, len + strlen(testFolder));
+    memcpy(path,paths,len * sizeof(char));
+    len = getLastOf(paths);
+    memcpy(path + len ,testFolder, strlen(testFolder) * sizeof(char));
+
 
     Top *top1;
     Crawl("kokekrewrkwel",path,&top1);
     TopPrl *top2;
     CrawlPrl("kokekrewrkwel",path,&top2);
     ASSERT_EQ(top1[2].levDistValue ,top2[2].levDistValue);
+    free(paths);
+    free(path);
     free(top1);
     free(top2);
 }
@@ -77,16 +88,19 @@ TEST(case3,test3)
 TEST(case4,test4){
     char *paths = get_current_dir_name();
     char *testFolder = (char*)"/files";
-    int len = getLastOf(paths);
+    int len = strlen(paths);
     char *path = (char*)malloc(sizeof(char) * (len + strlen(testFolder)));
-    memcpy(path,paths,strlen(paths));
-    memcpy(path + len,testFolder, len + strlen(testFolder));
+    memcpy(path,paths,len * sizeof(char));
+    len = getLastOf(paths);
+    memcpy(path + len ,testFolder, strlen(testFolder) * sizeof(char));
 
     Top *top1;
     Crawl("kokekrewrkwel",path,&top1);
     TopPrl *top2;
     CrawlPrl("kokekrewrkwel",path,&top2);
     ASSERT_EQ(top1[3].levDistValue, top2[3].levDistValue);
+    free(paths);
+    free(path);
     free(top1);
     free(top2);
 }
@@ -94,16 +108,19 @@ TEST(case4,test4){
 TEST(case5,test5){
     char *paths = get_current_dir_name();
     char *testFolder = (char*)"/files";
-    int len = getLastOf(paths);
+    int len = strlen(paths);
     char *path = (char*)malloc(sizeof(char) * (len + strlen(testFolder)));
-    memcpy(path,paths,strlen(paths));
-    memcpy(path + len,testFolder, len + strlen(testFolder));
+    memcpy(path,paths,len * sizeof(char));
+    len = getLastOf(paths);
+    memcpy(path + len ,testFolder, strlen(testFolder) * sizeof(char));
 
     Top *top1;
     Crawl("kokekrewrkwel",path,&top1);
     TopPrl *top2;
     CrawlPrl("kokekrewrkwel",path,&top2);
     ASSERT_EQ(top1[4].levDistValue, top2[4].levDistValue);
+    free(paths);
+    free(path);
     free(top1);
     free(top2);
 }
@@ -111,74 +128,92 @@ TEST(case5,test5){
 TEST(case6,test6){
     char *paths = get_current_dir_name();
     char *testFolder = (char*)"/files";
-    int len = getLastOf(paths);
+    int len = strlen(paths);
     char *path = (char*)malloc(sizeof(char) * (len + strlen(testFolder)));
-    memcpy(path,paths,strlen(paths));
-    memcpy(path + len,testFolder, len + strlen(testFolder));
+    memcpy(path,paths,len * sizeof(char));
+    len = getLastOf(paths);
+    memcpy(path + len ,testFolder, strlen(testFolder) * sizeof(char));
 
     Top *top1;
     Crawl("kokekrewrkwel",path,&top1);
     const char * t = "2.txt";
     ASSERT_EQ(*top1[3].fileName, *t);
+    free(paths);
+    free(path);
     free(top1);
+
 }
 
 TEST(case7,test7){
     char *paths = get_current_dir_name();
     char *testFolder = (char*)"/files";
-    int len = getLastOf(paths);
+    int len = strlen(paths);
     char *path = (char*)malloc(sizeof(char) * (len + strlen(testFolder)));
-    memcpy(path,paths,strlen(paths));
-    memcpy(path + len,testFolder, len + strlen(testFolder));
+    memcpy(path,paths,len * sizeof(char));
+    len = getLastOf(paths);
+    memcpy(path + len ,testFolder, strlen(testFolder) * sizeof(char));
+
 
     Top *top1;
     Crawl("kokekrewrkwel",path,&top1);
     const char * t = "10.txt";
     ASSERT_EQ(*top1[0].fileName, *t);
     free(top1);
+    free(paths);
+    free(path);
 }
 
 TEST(case8,test8){
     char *paths = get_current_dir_name();
     char *testFolder = (char*)"/files";
-    int len = getLastOf(paths);
+    int len = strlen(paths);
     char *path = (char*)malloc(sizeof(char) * (len + strlen(testFolder)));
-    memcpy(path,paths,strlen(paths));
-    memcpy(path + len,testFolder, len + strlen(testFolder));
+    memcpy(path,paths,len * sizeof(char));
+    len = getLastOf(paths);
+    memcpy(path + len ,testFolder, strlen(testFolder) * sizeof(char));
 
     TopPrl *top2;
     CrawlPrl("kokekrewrkwel",path,&top2);
     const char *t = "2.txt";
     ASSERT_EQ(*top2[3].fileName, *t);
     free(top2);
+    free(paths);
+    free(path);
 }
 
 TEST(case9,test9){
     char *paths = get_current_dir_name();
     char *testFolder = (char*)"/files";
-    int len = getLastOf(paths);
+    int len = strlen(paths);
     char *path = (char*)malloc(sizeof(char) * (len + strlen(testFolder)));
-    memcpy(path,paths,strlen(paths));
-    memcpy(path + len,testFolder, len + strlen(testFolder) );
+    memcpy(path,paths,len * sizeof(char));
+    len = getLastOf(paths);
+    memcpy(path + len ,testFolder, strlen(testFolder) * sizeof(char));
 
     Top *top1;
     Crawl("kokekrewrkwel",path,&top1);
     const char *t = "4.txt";
     ASSERT_EQ(*top1[1].fileName, *t);
     free(top1);
+    free(paths);
+    free(path);
 }
 
 TEST(case10,test10){
     char *paths = get_current_dir_name();
     char *testFolder = (char*)"/files";
-    int len = getLastOf(paths);
+    int len = strlen(paths);
     char *path = (char*)malloc(sizeof(char) * (len + strlen(testFolder)));
-    memcpy(path,paths,strlen(paths));
-    memcpy(path + len,testFolder, len + strlen(testFolder));
+    memcpy(path,paths,len * sizeof(char));
+    len = getLastOf(paths);
+    memcpy(path + len ,testFolder, strlen(testFolder) * sizeof(char));
+
 
     TopPrl *top2;
     CrawlPrl("kokekrewrkwel",path,&top2);
     const char *t = "3.txt";
     ASSERT_EQ( *top2[4].fileName, *t);
     free(top2);
+    free(paths);
+    free(path);
 }
