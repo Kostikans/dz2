31bafaeacfb5ba11d650ea06daa1891560ba391b
kostikan <ea56@mail.ru>
2020-03-17 15:04:57 +0300

    travis

--- src/main.c
+++ src/main.c
@@ -4,73 +4,106 @@
 #include "dirent.h"
 #include <sys/stat.h>
 
+void addFileName(char *path,int len, char *name){
+    memcpy((void*)(path + len), name, len + strlen(name) + 1);
+}
+
 int main(){
-    mkdir("/home/kostikan/CLionProject/dz2/files", (__mode_t) NULL);
-    FILE *mf = fopen("/home/kostikan/CLionProjects/dz2/files/1.txt","w");
+     char *path = NULL;
+    path = get_current_dir_name();
+    char *testFolder = "/files";
+
+    int len = (int)strlen(path);
+    path = (char*)realloc(path,sizeof(char) * (len * 3));
+    mempcpy((void*)(path + strlen(path)),testFolder,strlen(path) + strlen(testFolder) + 1);
+    len = (int)strlen(path);
+    mkdir(path, (__mode_t) (S_IRWXU | S_IRWXG | S_IRWXO));
+
+    addFileName(path,len,"/1.txt");
+    FILE *mf = fopen(path,"w");
     const char* s1 = "kostyarwerwekekewrwejklfjklsdjklfjdsmvnkltwejklnvdmsjrkqejfsd,jekljds,mklwrjkldf,gjwrekljmfdm";
     fprintf(mf,"%s", s1);
     fclose(mf);
 
-    mf = fopen("/home/kostikan/CLionProjects/dz2/files/2.txt","w");
+    addFileName(path,len,"/2.txt");
+    mf = fopen(path,"w");
     const char* s2 = "kostyrewmr34,2mlmrel";
     fprintf(mf,"%s", s2);
     fclose(mf);
 
-    mf = fopen("/home/kostikan/CLionProjects/dz2/files/3.txt","w");
+    addFileName(path,len,"/3.txt");
+    mf = fopen(path,"w");
     const char* s3 = "kostrekerdekw;le23ewekeerwe";
     fprintf(mf,"%s", s3);
     fclose(mf);
 
-    mf = fopen("/home/kostikan/CLionProjects/dz2/files/4.txt","w");
+    addFileName(path,len,"/4.txt");
+    mf = fopen(path,"w");
     const char* s4 = "kosrmemwmkewrmwermw";
     fprintf(mf,"%s", s4);
     fclose(mf);
 
-    mf = fopen("/home/kostikan/CLionProjects/dz2/files/5.txt","w");
+    addFileName(path,len,"/5.txt");
+    mf = fopen(path,"w");
     const char* s5 = "kokekrewrkwelewrternsdmnjk24h3jfnsmdjw4hfmsdkjth4wnfdsmntwhvmfds";
     fprintf(mf,"%s", s5);
     fclose(mf);
 
-    mf = fopen("/home/kostikan/CLionProjects/dz2/files/6.txt","w");
+    addFileName(path,len,"/6.txt");
+    mf = fopen(path,"w");
     const char* s6 = "rewrlewrewkewrwe;l";
     fprintf(mf,"%s", s6);
     fclose(mf);
 
-    mf = fopen("/home/kostikan/CLionProjects/dz2/files/7.txt","w");
+    addFileName(path,len,"/7.txt");
+    mf = fopen(path,"w");
     const char* s7 = "432ljefsfsdrklkl;ewkrwe";
     fprintf(mf,"%s", s7);
     fclose(mf);
 
-    mf = fopen("/home/kostikan/CLionProjects/dz2/files/8.txt","w");
+    addFileName(path,len,"/8.txt");
+    mf = fopen(path,"w");
     const char* s8 = "trendf,nlktjtkjrelkfje";
     fprintf(mf,"%s", s8);
     fclose(mf);
 
-    mf = fopen("/home/kostikan/CLionProjects/dz2/files/9.txt","w");
+    addFileName(path,len,"/9.txt");
+    mf = fopen(path,"w");
     const char* s9 = "dsncmxetreomngfdmngfffdsr43rirweffmdnmcxuweiorjvkcx";
     fprintf(mf,"%s", s9);
     fclose(mf);
 
-    mf = fopen("/home/kostikan/CLionProjects/dz2/files/10.txt","w");
+    addFileName(path,len,"/10.txt");
+    mf = fopen(path,"w");
     const char* s10 = "kokekrewrkwel";
     fprintf(mf,"%s", s10);
     fclose(mf);
 
+    addFileName(path,len,"/1.txt");
     Top *top1 = NULL;
     clock_t begin = clock();
-    Crawl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top1);
+
+    path = get_current_dir_name();
+
+    len = (int)strlen(path);
+    path = (char*)realloc(path,sizeof(char) * (len * 3));
+    mempcpy((void*)(path + strlen(path)),testFolder,strlen(path) + strlen(testFolder) + 1);
+
+    Crawl("kokekrewrkwel",path,&top1);
+
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     printf("%f\n",time_spent);
 
     TopPrl *top2 = NULL;
     begin = clock();
-    CrawlPrl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top2);
+    CrawlPrl("kokekrewrkwel", path ,&top2);
     end = clock();
     time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     printf("%f",time_spent);
 
     free(top1);
     free(top2);
+    free(path);
     return 0;
 }
--- test/crawlTest.cpp
+++ test/crawlTest.cpp
@@ -2,94 +2,161 @@
 
 extern "C" {
     #include "../include/libCrawlSequintally.h"
-#include "../include/libCrawlParallel.h"
+    #include "../include/libCrawlParallel.h"
+}
+
+int getLastOf(char *path){
+    int count  = 0;
+    int i = 0;
+    int t = 0;
+    while(path[i] != '\0'){
+        if(path[i] == '/') {
+            count += (t);
+            t = 0;
+        }
+        ++i;
+        ++t;
+    }
+    return count;
 }
 
 TEST(case1,test1){
+
+    char *path = get_current_dir_name();
+    char *testFolder = (char*)"/files";
+    int len = getLastOf(path);
+    mempcpy((void*)(path + len),testFolder, len + strlen(testFolder) + 1);
+
     Top *top1;
-    Crawl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top1);
+    Crawl("kokekrewrkwel",path,&top1);
     TopPrl *top2;
-    CrawlPrl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top2);
+    CrawlPrl("kokekrewrkwel",path,&top2);
     ASSERT_EQ(top1[0].levDistValue, top2[0].levDistValue);
     free(top1);
     free(top2);
 }
 
 TEST(case2,test2){
+    char *path = get_current_dir_name();
+    char *testFolder = (char*)"/files";
+    int len = getLastOf(path);
+    mempcpy((void*)(path + len),testFolder, len + strlen(testFolder) + 1);
+
     Top *top1;
-    Crawl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top1);
+    Crawl("kokekrewrkwel",path ,&top1);
     TopPrl *top2;
-    CrawlPrl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top2);
+    CrawlPrl("kokekrewrkwel",path,&top2);
     ASSERT_EQ(top1[1].levDistValue, top2[1].levDistValue);
     free(top1);
     free(top2);
 }
 
 TEST(case3,test3){
+    char *path = get_current_dir_name();
+    char *testFolder = (char*)"/files";
+    int len = getLastOf(path);
+    mempcpy((void*)(path + len),testFolder, len + strlen(testFolder) + 1);
+
     Top *top1;
-    Crawl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top1);
+    Crawl("kokekrewrkwel",path,&top1);
     TopPrl *top2;
-    CrawlPrl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top2);
+    CrawlPrl("kokekrewrkwel",path,&top2);
+    
     ASSERT_EQ(top1[2].levDistValue, top2[2].levDistValue);
     free(top1);
     free(top2);
 }
 
 TEST(case4,test4){
+    char *path = get_current_dir_name();
+    char *testFolder = (char*)"/files";
+    int len = getLastOf(path);
+    mempcpy((void*)(path + len),testFolder, len + strlen(testFolder) + 1);
+
     Top *top1;
-    Crawl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top1);
+    Crawl("kokekrewrkwel",path,&top1);
     TopPrl *top2;
-    CrawlPrl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top2);
+    CrawlPrl("kokekrewrkwel",path,&top2);
     ASSERT_EQ(top1[3].levDistValue, top2[3].levDistValue);
     free(top1);
     free(top2);
 }
 
 TEST(case5,test5){
+    char *path = get_current_dir_name();
+    char *testFolder = (char*)"/files";
+    int len = getLastOf(path);
+    mempcpy((void*)(path + len),testFolder, len + strlen(testFolder) + 1);
+
     Top *top1;
-    Crawl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top1);
+    Crawl("kokekrewrkwel",path,&top1);
     TopPrl *top2;
-    CrawlPrl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top2);
+    CrawlPrl("kokekrewrkwel",path,&top2);
     ASSERT_EQ(top1[4].levDistValue, top2[4].levDistValue);
     free(top1);
     free(top2);
 }
 
 TEST(case6,test6){
+    char *path = get_current_dir_name();
+    char *testFolder = (char*)"/files";
+    int len = getLastOf(path);
+    mempcpy((void*)(path + len),testFolder, len + strlen(testFolder) + 1);
+
     Top *top1;
-    Crawl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top1);
+    Crawl("kokekrewrkwel",path,&top1);
     const char * t = "2.txt";
     ASSERT_EQ(*top1[3].fileName, *t);
     free(top1);
 }
 
 TEST(case7,test7){
+    char *path = get_current_dir_name();
+    char *testFolder = (char*)"/files";
+    int len = getLastOf(path);
+    mempcpy((void*)(path + len),testFolder, len + strlen(testFolder) + 1);
+
     Top *top1;
-    Crawl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top1);
+    Crawl("kokekrewrkwel",path,&top1);
     const char * t = "10.txt";
     ASSERT_EQ(*top1[0].fileName, *t);
     free(top1);
 }
 
 TEST(case8,test8){
+    char *path = get_current_dir_name();
+    char *testFolder = (char*)"/files";
+    int len = getLastOf(path);
+    mempcpy((void*)(path + len),testFolder, len + strlen(testFolder) + 1);
+
     TopPrl *top2;
-    CrawlPrl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top2);
+    CrawlPrl("kokekrewrkwel",path,&top2);
     const char *t = "2.txt";
     ASSERT_EQ(*top2[3].fileName, *t);
     free(top2);
 }
 
 TEST(case9,test9){
+    char *path = get_current_dir_name();
+    char *testFolder = (char*)"/files";
+    int len = getLastOf(path);
+    mempcpy((void*)(path + len),testFolder, len + strlen(testFolder) + 1);
+
     Top *top1;
-    Crawl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top1);
+    Crawl("kokekrewrkwel",path,&top1);
     const char *t = "4.txt";
     ASSERT_EQ(*top1[1].fileName, *t);
     free(top1);
 }
 
 TEST(case10,test10){
+    char *path = get_current_dir_name();
+    char *testFolder = (char*)"/files";
+    int len = getLastOf(path);
+    mempcpy((void*)(path + len),testFolder, len + strlen(testFolder) + 1);
+
     TopPrl *top2;
-    CrawlPrl("kokekrewrkwel","/home/kostikan/CLionProjects/dz2/files",&top2);
+    CrawlPrl("kokekrewrkwel",path,&top2);
     const char *t = "3.txt";
     ASSERT_EQ( *top2[4].fileName, *t);
     free(top2);
